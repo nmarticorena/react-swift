ec4ced57890ed1682571557467cb079fa9da0cc2
jhavl <jhavl@users.noreply.github.com>
2021-09-06 16:09:15 +1000

    remove lazy loader

--- src/components/SwiftComponents.tsx
+++ src/components/SwiftComponents.tsx
@@ -4,7 +4,8 @@ import { useThree, useFrame } from '@react-three/fiber'
 import { PerspectiveCamera, useProgress, Html } from '@react-three/drei'
 
 THREE.Object3D.DefaultUp.set(0, 0, 1)
-const Loader = lazy(() => import('./Loader'))
+// const Loader = lazy(() => import('./Loader'))
+import Loader from './Loader'
 
 export const Plane: React.FC = (): JSX.Element => {
     const { scene } = useThree()
